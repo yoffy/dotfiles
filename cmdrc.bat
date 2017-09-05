780b929d39b897c3577232ddc5eb4420fdac10f6
Kenta Yoshimura <yottui@yahoo.co.jp>
2017-09-06 04:49:54 +0900

    add cmdrc.

---
+++ cmdrc.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+
+set PATH=%DevEnvDir%CommonExtensions\Microsoft\CMake\CMake\bin;%PATH%
